158e266b457b0f0df2790919b8387df5f0ad36b9
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-04-09 16:49:31 +0200

    first commit

---
+++ esercizio2.php
@@ -0,0 +1,84 @@
+<?php
+
+$squadre = [
+    'Inter' => ['Sommer,', 'Pavard,', 'Acerbi,', 'Bastoni,', 'Dumfries,', 'Barella,', 'Calhanoglu,', 'Mkhitaryan,', 'Dimarco,', 'Thuram,', 'Lautaro Martinez'],
+    'Milan' => ['Maignan,', 'Calabria,', 'Kjaer,', 'Tomori,', 'Theo Hernandez,', 'Bennacer,', 'Reijnders,', 'Pulisic,', 'Loftus-Cheek,', 'Leao,', 'Giroud'],
+    'Juventus'=> ['Szczesny,', 'Gatti,', 'Bremer,', 'Danilo,', 'Cambiaso,', 'McKennie,', 'Locatelli,', 'Rabiot,', 'Kostic,', 'Chiesa,', 'Vlahovic'],
+    'Bologna'=> ['Skorupski,', 'Posch,', 'Beukema,', 'Calafiori,', 'Kristiansen,', 'Moro,', 'Freuler,', 'Orsolini,', 'Ferguson,', 'Saelemaekers,', 'Zirkzee'],
+    'Roma'=> ['Rui Patricio,', 'Karsdorp,', 'Llorente,', 'Mancini,', 'ANGELINO,', 'Paredes,', 'Cristante,', 'Pellegrini,', 'Dybala,', 'El Shaarawy,', 'Lukaku'],
+    'Atalanta'=> ['Carnesecchi,', 'Scalvini,', 'Djimsiti,', 'Kolasinac,', 'Holm,', 'De Roon,', 'Ederson,', 'Ruggeri,', 'Koopmeiners,','Lookman,', 'Scamacca'],
+    'Napoli'=> ['Meret,', 'Mario Rui,', 'Rrahmani,', 'Juan Jesus,', 'Di Lorenzo,', 'Zielinski,', 'Lobotka,', 'Anguissa,', 'Politano,', 'Osimhen,', 'Kvaratskhelia'],
+    'Lazio'=> [ 'Provedel,', 'Lazzari,', 'Patric,', 'Romagnoli,', 'Marusic,', 'Guendouzi,', 'Rovella,', 'Luis Alberto,', 'Felipe Anderson,', 'Immobile,', 'Zaccagni'],
+    'Torino'=> ['Milinkovic-Savic,', 'Djidji,', 'Buongiorno,', 'Rodriguez,', 'Bellanova,', 'Ricci,', 'Ilic,', 'Lazaro,', 'Vlasic,', 'Sanabria,', 'Zapata'],
+    'Fiorentina'=> ['Terracciano,', 'Kayode,', 'Milenkovic,','Martinez Quarta,', 'Biraghi,', 'Arthur,', 'Duncan,', 'Gonzalez,', 'Bonaventura,', 'Ikoné,', 'Beltran'],
+    'Monza'=> ['Di Gregorio,', "D'Ambrosio", 'Pablo Marí,', 'Caldirola,', 'Ciurria,', 'Gagliardini,', 'Pessina,', 'Kyriakopoulos,', 'Colpani,', 'V. Carboni,', 'Mota'],
+    'Genoa'=> ['Martinez,', 'De Winter,', 'Bani,', 'Vasquez,', 'Sabelli,', 'Malinovskyi,', 'Badelj,', 'Frendrup,', 'Martin,', 'Gudmundsson,', 'Retegui'],
+    'Cagliari'=> [ 'Scuffet,', 'Zappa,', 'Wieteska,', 'Dossena,', 'Azzi,','Nandez,', 'Prati,', 'Makoumbou,', 'Viola,', 'Petagna,', 'Luvumbo'],
+    'Lecce'=> ['Falcone,', 'Gendrey,', 'Baschirotto,', 'Pongracic,', 'Gallo,', 'Kaba,', 'Ramadani,', 'Oudin,', 'Almqvist,', 'Krstovic,', 'Banda'],
+    'Udinese'=> ['Okoye,', 'Ferreira,', 'Perez,', 'Kristensen,', 'Ebosele,', 'Samardzic,', 'Walace,', 'Lovric,', 'Kamara,', 'Pereyra,', 'Lucca'],
+    'Empoli'=> ['Caprile,', 'Walukiewicz,', 'Ismajli,', 'Luperto,', 'Bereszynski,', 'ZURKOWSKI,', 'Grassi,', 'Marin,', 'Gyasi,', 'Cambiaghi,', 'NIANG'],
+'Hellas Verona'=> ['Montipò,', 'Tchatchoua,', 'Dawidowicz,', 'Magnani,', 'VINAGRE,', 'Duda,', 'Folorunsho,', 'NOSLIN,', 'Suslov,', 'Lazovic,', 'SWIDERSKI'],
+'Frosinone'=> ['Turati,', 'ZORTEA,', 'Monterisi,', 'Okoli,', 'VALERI,', 'Mazzitelli,', 'Barrenechea,', 'Harroui,', 'Soulé,', 'Kaio Jorge,', 'Reinier'],
+'Sassuolo'=> ['Consigli,', 'Toljan,', 'Erlic,', 'Ferrari,', 'DOIG,', 'Mateus Henrique,', 'Boloca,', 'Berardi,', 'Thorstvedt,', 'Laurienté,', 'Pinamonti'],
+'Salernitana'=> ['Costil,','Zanoli,','Manolas,', 'Pirola,','Bradaric,','Tchaouna,',  'Maggiore,','Coulibaly,','Vignato,','Candreva,','Simy,']
+];
+
+
+
+
+$partite = [
+    "1)" => ["Salernitana - Sassuolo   2-2"],
+    "2)" => ["Milan - Lecce   3-0"],
+    "3)" => ["Roma - Lazio   1-0"],
+    "4)" => ["Empoli - Torino   3-2"],
+    "5)" => ["Frosinone - Bologna   0-0"],
+    "6)" => ["Monza - Napoli   2-4"],
+    "7)" => ["Cagliari - Atalanta   2-1"],
+    "8)" => ["Hellas Verona - Genoa   1-2"],
+    "9)" => ["Juventus - Fiorentina   1-0"],
+    "10)" => ["Udinese - Inter   1-2"],
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Giornata 31</h2>
+
+<ul><?php
+        foreach($partite as $game => $match){?>
+    <li>
+        <?=$game ?>
+        <?php 
+            foreach($match as $squad){?>
+            <?= $squad ?><?php
+            }?>
+    </li><?php
+    }?>
+
+</ul>
+
+
+    <h1>Classifica</h1>
+    <ol><?php
+        foreach($squadre as $squad_name => $squad_form){?>
+        <li>
+            <h4>
+                <?= $squad_name?>
+            </h4>
+            <p><?php
+                foreach($squad_form as $player){?>
+                <?= $player ?><?php
+                } ?>
+            </p>
+        </li><?php
+    }?>
+    </ol>
+</body>
+</html>
